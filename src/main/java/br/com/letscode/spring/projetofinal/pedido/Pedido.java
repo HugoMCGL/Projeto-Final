b80c760f17d52d0b842b8fc675f759b1d483f220
Hugo <hugomarquescgl@gmail.com>
2021-11-11 18:06:55 -0300

    Implementação da Tabela Pedidos

--- src/main/java/br/com/letscode/spring/projetofinal/pedido/Pedido.java
+++ src/main/java/br/com/letscode/spring/projetofinal/pedido/Pedido.java
@@ -1,7 +1,28 @@
 package br.com.letscode.spring.projetofinal.pedido;
 
 
+import lombok.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+
 public class Pedido {
+    @Id
+    private Long numeroPedido;
 
+    @Column(nullable = false)
+    private String cliente;
 
+    @Column(nullable = false)
+    private String saborPizza;
+
+    @Column(nullable = false)
+    private Double valor;
 }
