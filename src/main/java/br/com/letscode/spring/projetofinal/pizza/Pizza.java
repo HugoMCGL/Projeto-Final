9ad7fae6b91a3828bab51b73f83509430f5e4d4a
Bixomin <guprado.freitas@gmail.com>
2021-11-11 17:05:56 -0300

    classe pizza

--- src/main/java/br/com/letscode/spring/projetofinal/pizza/Pizza.java
+++ src/main/java/br/com/letscode/spring/projetofinal/pizza/Pizza.java
@@ -1,4 +1,24 @@
 package br.com.letscode.spring.projetofinal.pizza;
 
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
 public class Pizza {
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String sabor;
+
+    @Column(nullable = false)
+    private double preco;
 }
