2cea7d95cbe52505f634702670ca9352b057e8cb
Gustavo Nascimento <gunasc2014@gmail.com>
2021-11-11 20:03:01 -0300

    adicionando swagger e criando restController de Cliente

--- src/main/java/br/com/letscode/spring/projetofinal/cliente/ClienteRestController.java
+++ src/main/java/br/com/letscode/spring/projetofinal/cliente/ClienteRestController.java
@@ -1,4 +1,35 @@
 package br.com.letscode.spring.projetofinal.cliente;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("clientes")
+@RestController
 public class ClienteRestController {
+    private final ClienteRepository clienteRepository;
+
+    public ClienteRestController(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    @GetMapping
+    public List<Cliente> getClientes() {
+        return this.clienteRepository.findAll();
+    }
+
+    @GetMapping("/{telefone}")
+    public Cliente getByTelefone(@PathVariable Long telefone) {
+        return this.clienteRepository.getById(telefone);
+    }
+
+    @PostMapping
+    public void cadastrar(@RequestBody Cliente cliente) {
+        this.clienteRepository.save(cliente);
+    }
+
+    @DeleteMapping("/{telefone}")
+    public void deletar(@PathVariable Long telefone) {
+        this.clienteRepository.deleteById(telefone);
+    }
 }
